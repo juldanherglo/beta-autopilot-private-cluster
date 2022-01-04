79d97b7819b84d5792741c2a60c13499f9a730a9
Julius Daniel Herrera Glomm <julius.daniel.herrera.glomm@gmail.com>
2022-01-04 14:03:19 +0100

    fix: remove master_auth section
    Basic authentication was removed for GKE cluster versions >= 1.19. If
    master_auth is present in your config with only password or username
    set, you can remove the block entirely.

--- cluster.tf
+++ cluster.tf
@@ -66,15 +66,6 @@ resource "google_container_cluster" "primary" {
     }
   }
 
-  master_auth {
-    username = var.basic_auth_username
-    password = var.basic_auth_password
-
-    client_certificate_config {
-      issue_client_certificate = var.issue_client_certificate
-    }
-  }
-
   addons_config {
     http_load_balancing {
       disabled = ! var.http_load_balancing
